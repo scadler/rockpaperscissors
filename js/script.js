4dca3ec95d3f260a76284555534c8eeae4eb4015
Scott Adler <anorangepi@gmail.com>
2018-10-17 04:25:03 +0000

    pictures

--- js/script.js
+++ js/script.js
@@ -13,24 +13,57 @@ var computerChoice = ""
 var randomNumber = 0
 var userWin = 0
 var compWin = 0
+    
 
 $(document).ready(function(){
+    // I am trying to hide these images and make them show
+    //when the relating word is typed
+    $("#stone1").hide();
+    $("#blade1").hide();
+    $("#parchment1").hide();
+    $("#stone2").hide();
+    $("#blade2").hide();
+    $("#parchment2").hide();
+    // Button clickhandler
 $("#shoot").click(function(){
     userChoice = $("#input").val();
     $("#userChoice").text($("#input").val());
     randomNumber = Math.random()
+    //This does not work Fix everthing below
+    if (userChoice = "Rock"){
+        $("#stone1").show();
+        $("#blade1").hide();
+        $("#parchment1").hide();
+    }
+    if (userChoice = "Paper"){
+        $("#stone1").hide();
+        $("#blade1").show();
+        $("#parchment1").hide();
+    }
+    if (userChoice = "Scissors"){
+        $("#stone1").hide();
+        $("#blade1").hide();
+        $("#parchment1").show();
+    }
+    //Fix everything above
     // Computer output
     if (randomNumber <= 0.33) {
         computerChoice = "Rock"
-        $("#computerChoice").html("Rock");
+        $("#stone2").show();
+        $("#blade2").hide();
+        $("#parchment2").hide();
     }
     if (randomNumber > 0.33 && randomNumber < 0.66) {
         computerChoice = "Scissors"
-        $("#computerChoice").html("Scissors");
+        $("#blade2").show();
+        $("#stone2").hide();
+        $("#parchment2").hide();
     }
     if (randomNumber >= 0.66) {
         computerChoice = "Paper"
-        $("#computerChoice").html("Paper");
+        $("#parchment2").show();
+        $("#stone2").hide();
+        $("#blade2").hide();
     }
     // Who wins
     if (userChoice === "Rock" && computerChoice === "Paper") {
@@ -76,5 +109,5 @@ $("#shoot").click(function(){
     
     console.log(userChoice);
 });
-
 });
+
